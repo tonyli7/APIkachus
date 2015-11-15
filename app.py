d8fa0747d7bd773f081b2e51cb5a19b9fb4105a6
kahsoonyap <kahsoonyap@users.noreply.github.com>
2015-11-15 13:55:32 -0500

    Moved changes from my branch to master.

--- app.py
+++ app.py
@@ -36,7 +36,11 @@ def pokemon(tag=""):
         request_url = urllib2.urlopen(url)
         result = request_url.read()
         r = json.loads(result)
-        return render_template("tagged.html", r=r['name'].lower())
+        return render_template("tagged.html",
+                               types=r['types'],
+                               name=r['name'].lower(),
+                               species=r['species'],
+                               move=r["moves"][(random.randrange(len(r["moves"])))]['name'].lower())
    # abilities=[]
    # for item in r['abilities']:
    #     try:
